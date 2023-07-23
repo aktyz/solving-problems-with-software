05933240033ac94fdab00b356bafbd8237c7129b
Zyta <zyta.slowianska@gmail.com>
2023-07-23 11:17:48 +0200

    feat: Print all genes for a given string

--- src/main/java/org/coursera/duke/java/week2/AllGenes.java
+++ src/main/java/org/coursera/duke/java/week2/AllGenes.java
@@ -10,18 +10,22 @@ public class AllGenes {
                 currentIndex = dna.indexOf(stopCodon, currentIndex + 1);
             }
         }
-        return dna.length();
+        return -1;
     }
 
-    public static String findGene(String dna) {
-        int startIndex = dna.indexOf("ATG");
-        if (startIndex == -1) return "";
-        int taaIndex = findStopCodon(dna, startIndex, "TAA");
-        int tagIndex = findStopCodon(dna, startIndex, "TAG");
-        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
-        int minIndex = Math.min(Math.min(taaIndex, tagIndex), tgaIndex);
-        if (minIndex == dna.length()) return "";
-        else return dna.substring(startIndex, minIndex + 3);
+    public static String findGene(String dna, int startIndex) {
+        int atgIndex = dna.indexOf("ATG", startIndex);
+        if (atgIndex == -1) return "";
+        int taaIndex = findStopCodon(dna, atgIndex, "TAA");
+        int tagIndex = findStopCodon(dna, atgIndex, "TAG");
+        int tgaIndex = findStopCodon(dna, atgIndex, "TGA");
+        int minIndex;
+        if(taaIndex == -1 || (tgaIndex != -1 && tgaIndex < taaIndex)) minIndex = tgaIndex;
+        else minIndex = taaIndex;
+        if(minIndex == -1 || (tagIndex != -1 && tagIndex < minIndex)) minIndex = tagIndex;
+
+        if (minIndex == -1) return "";
+        else return dna.substring(atgIndex, minIndex + 3);
     }
 
     public static void testFindStopCodon() {
@@ -34,33 +38,58 @@ public class AllGenes {
         if(dex != 24) System.out.println("error on 27");
 
         dex = findStopCodon(dna, 1, "TAA");
-        if(dex != 29) System.out.println("error on dna.length()");
+        if(dex != -1) System.out.println("error on dna.length()");
 
         dex = findStopCodon(dna, 0, "TAG");
-        if(dex != 29) System.out.println("error on dna.length()");
+        if(dex != -1) System.out.println("error on dna.length()");
         System.out.println("testFindStopCodon finished!");
+    }
+
+    public static void printAllGenes(String dna) {
+        int startIndex = 0;
+        String geneFound = "";
+        while( true ) {
+            geneFound = findGene(dna, startIndex);
+            if(geneFound.isEmpty()) break;
+            System.out.println(geneFound);
+            startIndex = dna.indexOf(geneFound, startIndex) + geneFound.length();
+        }
     }
 
     public static void testFindGene() {
         String dna = "AATGCGTAATATGGT";
-        if(!findGene(dna).equals("")) System.out.println("Error on first case");
+        if(!findGene(dna,0).equals("")) System.out.println("Error on first case");
 
         dna = "AATGCTAGGGTAATATGGT";
-        if(!findGene(dna).equals("ATGCTAGGGTAA")) System.out.println("Error on second case");
+        if(!findGene(dna,0).equals("ATGCTAGGGTAA")) System.out.println("Error on second case");
 
         dna = "AATGCTAGGTAATTGAGT";
-        if(!findGene(dna).equals("ATGCTAGGTAATTGA")) System.out.println("Error on third case");
+        if(!findGene(dna,0).equals("ATGCTAGGTAATTGA")) System.out.println("Error on third case");
 
         dna = "AATGCTAGGTAATCTGAGT";
-        if(!findGene(dna).equals("")) System.out.println("Error on fourth case");
+        if(!findGene(dna,0).equals("")) System.out.println("Error on fourth case");
 
         dna = "AATCGCTAGGTAATCTGAGT";
-        if(!findGene(dna).equals("")) System.out.println("Error on fifth case");
+        if(!findGene(dna,0).equals("")) System.out.println("Error on fifth case");
 
         System.out.println("testFindGene finished!");
     }
+
+    public static void testPrintAllGenes() {
+        //            ATG   TAA  ATG   v  v  TGA
+        String dna = "ATGctgTAAtaATGCTGCAACGGTGAAGA";
+        System.out.println("Testing printAllGenes on: " + dna);
+        printAllGenes(dna);
+        dna = "";
+        System.out.println("Testing printAllGenes on: " + dna);
+        printAllGenes(dna);
+        //     ATG   v  v  v  TAA   v  v  ATGTAA
+        dna = "ATGATCATAAGAAGATAATAGAGGGCCATGTAA";
+        System.out.println("Testing printAllGenes on: " + dna);
+        printAllGenes(dna);
+
+    }
     public static void main(String[] args) {
-        testFindStopCodon();
-        testFindGene();
+        testPrintAllGenes();
     }
 }
