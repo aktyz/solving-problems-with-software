c624a73363ed465b09d6c623e60a0edda62f442a
Zyta <zyta.slowianska@gmail.com>
2023-11-08 18:34:10 +0100

    feat: fileWithColdestTemperature()

--- src/main/java/org/coursera/duke/java/week3/WeatherCSVProblem.java
+++ src/main/java/org/coursera/duke/java/week3/WeatherCSVProblem.java
@@ -1,8 +1,11 @@
 package org.coursera.duke.java.week3;
 
+import edu.duke.DirectoryResource;
 import edu.duke.FileResource;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
 
 public class WeatherCSVProblem {
 
@@ -26,7 +29,52 @@ public class WeatherCSVProblem {
         CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
         System.out.println("coldest temperature was " + coldest.get("TemperatureF") + " at " + coldest.get("TimeEST"));
     }
+
+    public static String fileWithColdestTemperature() {
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord coldestRecord = null;
+        String fileName = null;
+        for(File f : dr.selectedFiles()) {
+            CSVParser parser = fileToParser(f);
+
+            if(coldestRecord == null) {
+                coldestRecord = coldestHourInFile(parser);
+                fileName = f.getName();
+            }
+            else {
+                CSVRecord currentFileColdestRecord = coldestHourInFile(parser);
+                if(Double.parseDouble(currentFileColdestRecord.get("TemperatureF")) < Double.parseDouble(coldestRecord.get("TemperatureF"))) {
+                    coldestRecord = currentFileColdestRecord;
+                    fileName = f.getName();
+                }
+            }
+        }
+        printColdestFileInfo(fileName, coldestRecord);
+        return fileName;
+    }
+
+    private static void printColdestFileInfo(String fileName, CSVRecord coldestRecord) {
+        System.out.println("Coldest day was in file " + fileName);
+        System.out.println("Coldest temperature on that day was " + coldestRecord.get("TemperatureF"));
+        System.out.println("All the Temperatures on the coldest day were:");
+        printAllFileTemperatures(fileName);
+    }
+
+    private static void printAllFileTemperatures(String fileName) {
+        String year = fileName.substring(8, 12);
+        FileResource fr = new FileResource("resources/week3/nc_weather/" + year + "/" + fileName);
+        CSVParser parser = fr.getCSVParser();
+        for(CSVRecord line : parser) {
+            System.out.println(line.get("DateUTC") + ": " + line.get("TemperatureF"));
+        }
+    }
+
+    public static CSVParser fileToParser(File f) {
+        FileResource fr = new FileResource(f);
+        return fr.getCSVParser();
+    }
     public static void main(String[] args) {
-        testColdestHourInFile();
+        //testColdestHourInFile();
+        fileWithColdestTemperature();
     }
 }
